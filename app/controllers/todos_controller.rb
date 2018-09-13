1ab8b3ece0c770f16b36348cd8ad5b580d2e1b3c
akasaMac <tarou_17n1101159@nnn.ed.jp>
2018-09-13 23:21:06 +0900

    can post some input params

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -11,28 +11,33 @@ class TodosController < ApplicationController
   def show
     @todo = Todo.find(params[:id])
     @image = Image.find_by(todo_id: @todo.id)
-    assign_meta_tags(
-      title: "Super Test",
-      site: @todo.twitter_id,
-      image: "https://secure-ridge-55094.herokuapp.com/assets/#{@image.name}",
-      url: "https://secure-ridge-55094.herokuapp.com/todos/#{@todo.id}"
-    )
+    # assign_meta_tags(
+    #   title: "Super Test",
+    #   site: @todo.twitter_id,
+    #   image: "https://secure-ridge-55094.herokuapp.com/assets/#{@image.name}",
+    #   url: "https://secure-ridge-55094.herokuapp.com/todos/#{@todo.id}"
+    # )
   end
 
   def create
-    todo = current_user.todos.create(
-      body: params[:post][:body],
-      twitter_id: current_user.twitter_id,
-      likes_count: 0
-    )
-    image = current_user.images.create(
-      twitter_id: current_user.twitter_id,
-      todo_id: todo.id,
-      name: "#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.png"
-    )
-    image.make(current_user.twitter_id,image.name,todo.body,current_user.icon_url)
-    # todo.tweet()
-    redirect_to todo_path(todo)
+    render plain: params[:post]
+    # todo = current_user.todos.create(
+    #   body: params[:post][:body],
+    #   twitter_id: current_user.twitter_id,
+    #   likes_count: 0
+    #  )
+    # image = current_user.images.create(
+    #   twitter_id: current_user.twitter_id,
+    #   todo_id: todo.id,
+    #   name: "#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.png"
+    #   )
+    # image.make(
+    #   current_user.twitter_id, 
+    #   image.name, todo.body, 
+    #   current_user.icon_url
+    #   )
+    # # todo.tweet()
+    # redirect_to todo_path(todo)
   end
 
   def destroy
