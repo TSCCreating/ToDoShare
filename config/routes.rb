521affedd543f8f59482d0c41824b8cdb407b3d5
akasaMac <tarou_17n1101159@nnn.ed.jp>
2018-09-18 20:45:02 +0900

    change label to false

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :todos, only: [:create, :destroy, :new, :show] do
+  resources :todos, only: [:index, :create, :destroy, :new, :show] do
     resources :likes, only: [:create, :destroy]
   end
   get '/top' => 'static_pages#top'
